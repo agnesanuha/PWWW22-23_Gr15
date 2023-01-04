5a6f22009f37df526692e961d8e64d349ccb29b3
agnesanuha <agnesanuha4@gmail.com>
2023-01-04 10:51:09 +0100

    add products page

---
+++ script.js
@@ -0,0 +1,16 @@
+function myFunction() {
+    var input, filter, itemsForSearch, a, i, txtValue;
+    input = document.getElementById("inputForSearch");
+    filter = input.value.toUpperCase();
+    itemsForSearch = document.getElementById("forSearch");
+    itemForSearch = itemsForSearch.getElementsByClassName("item");
+    for (i = 0; i < itemForSearch.length; i++) {
+        a = itemForSearch[i].getElementsByTagName("h5")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            itemForSearch[i].style.display = "";
+        } else {
+            itemForSearch[i].style.display = "none";
+        }
+    }
+}
